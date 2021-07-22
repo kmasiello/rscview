fda9de2c860fca29ace40a11558567420e224711
Richard Iannone <riannone@me.com>
2021-07-22 16:22:05 -0400

    Update create_pin.R

--- R/create_pin.R
+++ R/create_pin.R
@@ -1,22 +1,22 @@
-
 #' Create pin on Connect
 #'
 #' @param server Connect server address
 #' @param key Administrator API key
 #'
-#' @return
 #' @export
-
-# KM note - will want to update this with the dev version of pins at some point.
-create_pin <- function(x, name = deparse(substitute(x)),
+create_pin <- function(x,
+                       name = deparse(substitute(x)),
                        description = deparse(substitute(x)),
                        server = Sys.getenv("CONNECT_SERVER"),
                        key = Sys.getenv("CONNECT_API_KEY")) {
+
+  # TODO: When the next version of `pins` is released on CRAN,
+  # use the new implementation
+
   pins::board_register_rsconnect(
-    key = key,
-    server = server)
+    server = server,
+    key = key
+  )
 
   pins::pin(x, name = name, description = description, board = "rsconnect")
-
-
 }
