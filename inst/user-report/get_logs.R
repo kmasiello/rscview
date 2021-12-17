6e5af44e67c1bdc1cbbd8ebbfa0d875eb2802f45
kmasiello <32964481+kmasiello@users.noreply.github.com>
2021-12-17 13:41:38 -0800

    delete get_logs.R

--- inst/user-report/get_logs.R
+++
@@ -1,8 +0,0 @@
-conn <-
-  connectapi::connect(
-    host = Sys.getenv("CONNECT_SERVER"),
-    api_key = Sys.getenv("CONNECT_API_KEY")
-  )
-logs <- connectapi::get_audit_logs(conn, limit = Inf)
-filename <- paste0("logs_", lubridate::today(), ".rds")
-readr::write_rds(logs, file = filename)
