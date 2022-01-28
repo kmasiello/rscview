7ff66a11a5606fe5bc122097239c5d1cef05fc1d
kmasiello <32964481+kmasiello@users.noreply.github.com>
2022-01-28 12:24:15 -0800

    start audit tools

---
+++ R/audit.R
@@ -0,0 +1,9 @@
+#####To do
+# Audit functions to be used as part of a scheduled job
+#   audit_access - alert if content has an access permission
+#                 level outside of defined acceptable range. i.e., content set to access=all. can
+#                 refine to apply only to content with specific tag(s), or maybe other things
+#                 too.
+#   audit_self_add - alert if an admin has self-added to content
+#
+
