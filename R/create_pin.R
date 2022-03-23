2f4277aa0b5f1029cada662e1663fdfb35bc4587
Richard Iannone <riannone@me.com>
2022-03-23 16:31:58 -0400

    Update create_pin.R

--- R/create_pin.R
+++ R/create_pin.R
@@ -1,23 +1,27 @@
-#' Create pin on Connect
+#' Create a pin on Connect
 #'
-#' @param server Connect server address
-#' @param key Administrator API key
+#' Helper function used internally to pin audit data in a consistent manner.
 #'
-#' @export
-create_pin <- function(x,
-                       name = deparse(substitute(x)),
-                       description = deparse(substitute(x)),
-                       server = Sys.getenv("CONNECT_SERVER"),
-                       key = Sys.getenv("CONNECT_API_KEY"),
-                       metadata = list("server_audit" = TRUE)) {
+#' @param x An R object (e.g., data frame, list, etc.).
+#' @param server Connect server address.
+#' @param key Administrator API key.
+#'
+#' @noRd
+create_pin <- function(
+    x,
+    name = deparse(substitute(x)),
+    description = deparse(substitute(x)),
+    server = Sys.getenv("CONNECT_SERVER"),
+    key = Sys.getenv("CONNECT_API_KEY"),
+    metadata = list("server_audit" = TRUE)
+) {
 
-  # TODO: When the next version of `pins` is released on CRAN,
-  # use the new implementation
-
-  board <- pins::board_rsconnect(auth = "manual",
-    server = server,
-    key = key
-  )
+  board <-
+    pins::board_rsconnect(
+      auth = "manual",
+      server = server,
+      key = key
+    )
 
   pins::pin_write(board, x, name = name, metadata = metadata)
 }
