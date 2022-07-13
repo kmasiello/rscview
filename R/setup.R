98a61c451e3e754961e1c110e29b6ebff5dd161d
Richard Iannone <riannone@me.com>
2022-07-13 16:43:19 -0400

    Create setup.R

---
+++ R/setup.R
@@ -0,0 +1,36 @@
+#' Access to settings for the **rscview** package
+#'
+#' @description
+#' Access to settings for the **rscview** package.
+#'
+#' @param admin_username The Connect username which has admin privileges.
+#' @param file The filename for the YAML settings file.
+#'
+#' @export
+rscview_settings <- function(
+    admin_username,
+    file = "rscview_settings.yml"
+) {
+
+  # Get the
+
+
+
+  yaml_list <-
+    list(
+      admin_username = admin_username
+    )
+
+  # Write the YAML to the working directory (for now)
+  yaml::write_yaml(yaml_list, file = file)
+}
+
+# TODO: when the application setting directory is available then just
+# look at the first YAML file in the directory; provide an error if directory
+# or file is not available
+get_rsc_admin_username <- function(file) {
+
+  yaml_list <- yaml::read_yaml(file = file)
+
+  yaml_list$admin_username
+}
